432db19aa9ff939a80c011b35dcd36914f01f2a0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-11 20:26:29 +0300

    Update Task5.java

--- src/Homework_1/task_5/Task5.java
+++ src/Homework_1/task_5/Task5.java
@@ -1,13 +1,14 @@
 package Homework_1.task_5;
 
 public class Task5 {
-
+// пустая строка не нужна
     public static void main(String[] args) {
         // write your code here
         float sm =0;
         int dm;
+        // for (int dm=1; dm<=20;dm++) { - лучше так
         for (dm=1; dm<=20;dm++) {
-            sm = (float) (dm * 0.24);
+            sm = (float) (dm * 0.24); // результат можно оставить типом double
             System.out.println("Перевод от 1 до 20 дюймов в см: " + dm + " дюйм " + sm + " см");
         }
     }
