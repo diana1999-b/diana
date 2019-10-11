15cdfac071dd118abe9f20ac240db3e3a6595dba
diana1999-b <56068714+diana1999-b@users.noreply.github.com>
2019-10-11 15:27:53 +0300

    Task 5

---
+++ src/homework_1/Task_1/Task_5/Task5.java
@@ -0,0 +1,14 @@
+package homework_1.Task_1.Task_5;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        // write your code here
+        float sm =0;
+        int dm;
+        for (dm=1; dm<=20;dm++) {
+            sm = (float) (dm * 0.24);
+            System.out.println("Перевод от 1 до 20 дюймов в см: " + dm + " дюйм " + sm + " см");
+        }
+    }
+}
