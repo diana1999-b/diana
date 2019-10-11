344168e3328d8bba43759246810979f65d096c21
diana1999-b <56068714+diana1999-b@users.noreply.github.com>
2019-10-11 14:30:43 +0300

    Task 2

---
+++ src/Task_1/Task2.java
@@ -0,0 +1,16 @@
+package Task_1;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+            // write your code here
+            int t = 0; //task 2
+            int k=0;
+            while (t < 24)
+            {
+                t= t+3;
+                k=k+2;
+                System.out.println("Количество амеб:" + k +"при t:" + t);
+            }
+        }
+    }
