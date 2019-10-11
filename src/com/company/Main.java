0da907d301da8e6f5a98e96d73dbb0cfc7524833
diana1999-b <56068714+diana1999-b@users.noreply.github.com>
2019-10-11 12:55:55 +0300

    Task 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
-
-public class Main {
-
+public  class Main {
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        double t = 10;//1-ый день
+        double sum = 0;
+        for (int i = 1; i <= 7; i++) {
+            t = (t + t * 0.1);
+            sum = sum + t;
+        }
+     System.out.println("Суммарный путь за 7 дней : " + sum + "км");
     }
 }
+
