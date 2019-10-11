00c88262968f81329f96008e473dd7a5ec6be586
diana1999-b <56068714+diana1999-b@users.noreply.github.com>
2019-10-11 15:30:23 +0300

    Task 6

---
+++ src/Task_6/Task6.java
@@ -0,0 +1,13 @@
+package Task_6;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        // write your code here
+        int i;
+        for (i =1; i<=100; i++){
+            if (i%2==0)
+                System.out.println("Четное число: " + i);
+        }
+    }
+}
