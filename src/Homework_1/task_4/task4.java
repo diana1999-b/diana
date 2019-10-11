b5eb918b920b408c2271f6d33ab79a479eb39383
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-11 20:24:21 +0300

    Update task4.java

--- src/Homework_1/task_4/task4.java
+++ src/Homework_1/task_4/task4.java
@@ -2,12 +2,14 @@ package Homework_1.task_4;
 
 import java.util.Scanner;
 
+// task4 -> Task_4
 public class task4 {
 
         public static void main(String[] args) {
-            // write your code here
+            // write your code here - удаляй этот комментарий
             System.out.print("Введите A: ");
             Scanner in = new Scanner(System.in);
+                // a - плохое название переменной
             int a = in.nextInt();
             System.out.print("Введите B: ");
             int b = in.nextInt();
