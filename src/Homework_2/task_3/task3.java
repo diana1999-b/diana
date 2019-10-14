c4c82d83fa10cb75eed0d176084687a6d7258185
diana1999-b <56068714+diana1999-b@users.noreply.github.com>
2019-10-14 22:52:08 +0300

    task1

---
+++ src/Homework_2/task_3/task3.java
@@ -0,0 +1,28 @@
+package Homework_2.task_3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class task3 {
+
+    public static void main (String[] args)
+    {
+        /*Есть массив чисел. Заменить каждый нечетный элемент массива на 0*/
+        int size = 0;
+        System.out.println("Enter array size: ");
+        Scanner in = new Scanner(System.in);//получаем Input Stream
+        size = in.nextInt();//считываем примитивные типы данных (int)
+        int [] array = new int[size];//выделение памяти под массив
+        for (int i =0; i< array.length; i++)
+        {
+            System.out.println("Enter array element: ");
+            Scanner inArray = new Scanner(System.in);
+            array[i] = inArray.nextInt();
+            if (array[i]%2!=0)//проверка если остаток от деления не равен 0
+            {
+                array[i] = 0; //присваиваем значение элементу массива 0
+            }
+        }
+        System.out.println("Array: " + Arrays.toString(array));//вывод всех элементов массива
+    }
+}
