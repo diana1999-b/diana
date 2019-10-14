58c7929ef43c318cd7d122868e1c6a722c272ddd
diana1999-b <56068714+diana1999-b@users.noreply.github.com>
2019-10-14 22:26:17 +0300

    task1

---
+++ src/Homework_2/task_2/task2.java
@@ -0,0 +1,27 @@
+package Homework_2.task_2;
+
+        import java.util.Arrays;
+        import java.util.Scanner;
+
+public class task2 {
+
+    public static void main(String[] args)
+    {
+        /*Есть массив чисел. Найти среднее арифметическое число элементов массива*/
+        int size = 0;//размер массива
+        double average =0;//среднее арифметическое массива —присвоили значение 0
+        System.out.println("Enter array size:");
+        Scanner in = new Scanner(System.in);//получаем Input Stream
+        size = in.nextInt();//считываем примитивные типы данных (int)
+        int [] array = new int[size]; //выделение памяти для массива
+        for (int i=0; i< array.length; i++)
+        {
+            System.out.println("Enter array element: ");//введите элемент массива
+            Scanner inArray = new Scanner(System.in);//получаем Input Stream
+            array[i]= inArray.nextInt();//считываем примитивные типы данных (int)
+            average +=array[i];//подсчет сумы элементов массива
+        }
+        average = average/size;//определение среднего арифметического массива
+        System.out.println("Array average:" + average);
+    }
+}
