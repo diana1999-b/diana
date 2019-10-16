730a6398c7d6497984d9ca860d3a50b6a8d67966
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-16 21:04:29 +0300

    Update Task2.java

--- src/Homework_2/task_2/Task2.java
+++ src/Homework_2/task_2/Task2.java
@@ -2,7 +2,8 @@ package Homework_2.task_2;
 
         import java.util.Arrays;
         import java.util.Scanner;
-
+//форматируй код
+// старайся не оставлять комментарии в коде
 public class Task2 {
 
     public static void main(String[] args)
@@ -17,8 +18,10 @@ public class Task2 {
         for (int i=0; i< array.length; i++)
         {
             System.out.println("Enter array element: ");//введите элемент массива
+            // Scanner inArray = new Scanner(System.in) - один раз за пределами цикла
             Scanner inArray = new Scanner(System.in);//получаем Input Stream
             array[i]= inArray.nextInt();//считываем примитивные типы данных (int)
+            // ты оставляешь комментарий что считаешь сумму, а переменную назвала 'average', измени название
             average +=array[i];//подсчет сумы элементов массива
         }
         average = average/size;//определение среднего арифметического массива
